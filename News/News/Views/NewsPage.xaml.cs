fe3bd7dec0b328a97087d5495701bff9d6eed1c8
Jonathan <Jonathan@DESKTOP-CKOU825>
2022-04-14 16:44:04 +0200

    MyNewsApp

--- News/News/Views/NewsPage.xaml.cs
+++ News/News/Views/NewsPage.xaml.cs
@@ -15,6 +15,7 @@ namespace News.Views
     public partial class NewsPage : ContentPage
     {
         NewsService service;
+
         public NewsPage()
         {
             InitializeComponent();
@@ -38,8 +39,12 @@ namespace News.Views
                 Task<NewsGroup> t1 = service.GetNewsAsync(selectedCategory);
                 Device.BeginInvokeOnMainThread(() =>
                 {
-                    groupedList.ItemsSource = t1.Result.Articles;
-                    pageInfo.Text = $"{Title}";
+                    groupedList.ItemsSource = t1.Result.Articles.Skip(1);
+                    firstPageUrl.Source = t1.Result.Articles[0].UrlToImage;
+                    //firstPageDescription.Text = t1.Result.Articles[0].Description;
+                    firstPageDateTime.Text = t1.Result.Articles[0].DateTime.ToString("hh:mm - d");
+                    firstPageAuthor.Text = t1.Result.Articles[0].Author;
+                    firstPageTitle.Text = t1.Result.Articles[0].Title;
                 });
             });
         }
@@ -50,5 +55,50 @@ namespace News.Views
 
             await Navigation.PushAsync(new ArticleView(item.Url));
         }
+
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            var action = await DisplayActionSheet("Which Option would you like to select?", "Cancel", null, "Slow Internet", "Throw an error");
+
+            if (optionsButton.IsPressed)
+                ImageButton_Clicked(sender, e);
+
+            if (action != null)
+                await LoadNews();
+
+            if (action.Contains("Throw an error"))
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Error!", "You're not allowed to do that!", "Cancel");
+                }
+            }
+            if (action.Contains("Slow Internet"))
+                SlowInternetPressed();
+        }
+
+        private async void SlowInternetPressed()
+        {
+            slowedInternet.IsVisible = true;
+            slowedInternet.IsRunning = true;
+            await Task.Delay(4000);
+            await LoadNews();
+            slowedInternet.IsRunning = false;
+        }
+
+        private async void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            refreshProgress.Progress = 0;
+            refreshProgress.IsVisible = true;
+            await refreshProgress.ProgressTo(1, 2000, Easing.Linear);
+            refreshProgress.IsVisible = false;  
+            await LoadNews();
+        }
+
     }
 }
